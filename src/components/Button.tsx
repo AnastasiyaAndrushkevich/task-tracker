278513ff2668a6bad598e29ac50e85d7d6b56f40
Anastasiya Andrushkevich <nastyayarosh147@gmail.com>
2025-07-11 15:31:33 +0300

    Convert Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,11 @@
 import "./Button.css";
+import React from "react";
 
-export default function Button({ onClick }) {
+type ButtonProps = {
+  onClick: () => void; //Я ожидаю, что мне передадут onClick, и это будет функция, которая ничего не принимает и ничего не возвращает.
+};
+
+export default function Button({ onClick }: ButtonProps) {
   return (
     <button className="button" onClick={onClick}>
       Add task
