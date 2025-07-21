79288653d80852e2889f0da0b8b41219270541da
Anastasiya Andrushkevich <nastyayarosh147@gmail.com>
2025-07-21 18:19:23 +0300

    TaskPoint

--- src/components/TaskPoint.tsx
+++ src/components/TaskPoint.tsx
@@ -1,11 +1,75 @@
 import { useEffect, useState } from "react";
 import "./TaskPoint.css";
 import { TaskType } from "../types";
-import AddTaskForm from "./AddTaskForm";
-import TaskItem from "./TaskItem";
-import TaskList from "./TaskList";
-import TaskFilter from "./TaskFilter";
 
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../store/store";
+import {
+  toggleDone,
+  deleteTask,
+  startEdit,
+  cancelEdit,
+  changeEditedText,
+  saveEdit,
+  deleteCompleted,
+  setFilter,
+  setSearchTerm,
+} from "../store/tasksSlice";
+import TaskItem from "./TaskItem";
+import TaskFilter from "./TaskFilter";
+import AddTaskForm from "./AddTaskForm";
+
+export default function TaskPoint() {
+  const dispatch = useDispatch();
+  const { tasks, editIndex, editedText, filter, searchTerm } = useSelector(
+    (state: RootState) => state.tasks
+  );
+  const filteredTasks = tasks
+    .filter((task) => {
+      if (filter === "done") return task.done;
+      if (filter === "active") return !task.done;
+      return true;
+    })
+    .filter((task) =>
+      task.text.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  useEffect(() => {
+    localStorage.setItem("myTasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  return (
+    <div className="app-container">
+      <TaskFilter
+        filter={filter}
+        setFilter={(value) => dispatch(setFilter(value))}
+        searchTerm={searchTerm}
+        setSearchTerm={(term) => dispatch(setSearchTerm(term))}
+        onDeleteCompleted={() => dispatch(deleteCompleted())}
+      />
+
+      <ul>
+        {filteredTasks.map((item, index) => (
+          <TaskItem
+            key={index}
+            task={item}
+            index={index}
+            isEditing={editIndex === index}
+            editedText={editedText}
+            onChangeEditedText={(text) => dispatch(changeEditedText(text))}
+            onToggleDone={() => dispatch(toggleDone(index))}
+            onDelete={() => dispatch(deleteTask(index))}
+            onEdit={() => dispatch(startEdit(index))}
+            onSave={() => dispatch(saveEdit(index))}
+            onCancel={() => dispatch(cancelEdit())}
+          />
+        ))}
+      </ul>
+      {/* <AddTaskForm /> */}
+    </div>
+  );
+}
+
+/*
 export default function TaskPoint() {
   const [tasks, setTasks] = useState<TaskType[]>(() => {
     const saved = localStorage.getItem("myTasks"); //достать из браузера (localStorage) значение под именем майТаскс и сохранить это в переменную saved
@@ -88,3 +152,4 @@ export default function TaskPoint() {
     </div>
   );
 }
+*/
