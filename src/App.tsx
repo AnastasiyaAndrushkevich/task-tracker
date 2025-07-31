db1eb79fde8e02924d557e62e61188bde1800380
Anastasiya Andrushkevich <nastyayarosh147@gmail.com>
2025-07-31 19:08:57 +0300

    UI. Add welcomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import WelcomePage from "./components/WelcomePage/WelcomePage";
 import "./App.css";
 import Header from "./components/Header/Header";
 import TaskPoint from "./components/TaskPoint/TaskPoint";
 
 function App() {
   return (
-    <div className="app-container">
-      <Header />
-      <TaskPoint />
-    </div>
+    <Routes>
+      <Route path="/" element={<WelcomePage />} />
+      <Route path="/tasks" element={<TaskPoint />} />
+    </Routes>
   );
 }
 export default App;
